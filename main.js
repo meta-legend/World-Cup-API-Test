ac060a46f26c168f1016d6d4de84031f981ebc38
Pranab Shukla <pranab.austin@gmail.com>
2022-11-22 21:39:51 -0600

    adding second API to test

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const apiTestBtn = document.getElementById("api-input-btn");
 const apiTestResult = document.getElementById("api-test-result")
+const apiTestResult2 = document.getElementById("api-test-result-2")
 
 apiTestBtn.addEventListener("click", () => {
     fetch("http://api.cup2022.ir/api/v1/user/login", {
@@ -21,4 +22,24 @@ apiTestBtn.addEventListener("click", () => {
     }).then(data => {
         apiTestResult.innerHTML = data
     }); 
+    
+    fetch("https://reqres.in/api/login", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'email': 'example@gmail.com',
+            'password': 'memes'
+        })
+    }).then(res => { 
+        if (res.ok) {
+            console.log("success, status code: " + res.status)
+        } else {
+            console.log("failure, status code: " + res.status)
+        }
+        return res.json();
+    }).then(data => {
+        apiTestResult2.innerHTML = data
+    })
 });
